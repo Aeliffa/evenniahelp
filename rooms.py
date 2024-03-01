d0f415329d926102445c28042e62fb97ecb27ac9
Aeliffa <137568434+Aeliffa@users.noreply.github.com>
2024-03-01 13:29:59 -0800

    Add files via upload

---
+++ rooms.py
@@ -0,0 +1,26 @@
+"""
+Room
+
+Rooms are simple containers that has no location of their own.
+
+"""
+
+from evennia.objects.objects import DefaultRoom
+
+from .objects import ObjectParent
+
+from evennia.contrib.rpg.rpsystem.rpsystem import ContribRPRoom
+
+
+class Room(ObjectParent, ContribRPRoom):
+    """
+    Rooms are like any Object, except their location is None
+    (which is default). They also use basetype_setup() to
+    add locks so they cannot be puppeted or picked up.
+    (to change that, use at_object_creation instead)
+
+    See mygame/typeclasses/objects.py for a list of
+    properties and methods available on all Objects.
+    """
+
+    pass
